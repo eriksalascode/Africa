d668caae522ab0f6cc3124d52125c08b94feaf6c
Erik Salas <25646533+eriksalascode@users.noreply.github.com>
2021-05-06 10:44:56 -0500

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Erik Salas on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
