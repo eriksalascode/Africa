4cc71c8d6f2897c2d13d3342a87f846b920ff7da
Erik Salas <25646533+eriksalascode@users.noreply.github.com>
2021-05-18 14:22:07 -0500

    Build video tab view, subviews, and video playback functionality.

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct VideoListView: View {
+    //MARK: - PROPERTIES
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    //MARK: - BODY
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    NavigationLink(destination: VideoPlayerView(videoSelected: item.id, videoTitle: item.name)) {
+                        VideoListItemView(video: item)
+                            .padding(.vertical, 8)
+                    }
+                } //: ForEach
+            } //: List
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar(content: {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        //Shuffle Videos
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    }) {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            })
+        } //: NavigationView
+        
     }
 }
 
---
+++ Africa/Helper/VideoPlayerHelper.swift
@@ -0,0 +1,20 @@
+//
+//  VideoPlayerHelper.swift
+//  Africa
+//
+//  Created by Erik Salas on 5/18/21.
+//
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+    if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+        videoPlayer?.play()
+    }
+    
+    return videoPlayer!
+}
