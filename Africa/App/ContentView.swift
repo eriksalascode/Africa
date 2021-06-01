5beea1a789beb5f77fb77c81991af10efc69cbc7
Erik Salas <25646533+eriksalascode@users.noreply.github.com>
2021-05-31 21:58:37 -0500

    Complete all views, create grid, convert to mac enabled app, and make sticker pack

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -8,23 +8,100 @@
 import SwiftUI
 
 struct ContentView: View {
-    //MARK: - PROPERTIES
+    //MARK: - Properties
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    @State private var isGridViewActive: Bool = false
+    
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    //gridColumn will keep track of the next layout to be shown, which will be the current image shown
+    @State private var gridColumn: Int = 1
+    @State private var toolbarIcon: String = "square.grid.2x2"
+    
+    //MARK: - Functions
+    func gridSwitch() {
+        /*
+         calculating columns using the % operator
+         1 % 3 = 1 -> 1 + 1 = 2
+         2 % 3 = 2 -> 2 + 1 = 3
+         3 % 3 = 0 -> 0 + 1 = 1
+         */
+        gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+        gridColumn = gridLayout.count
+        print("Grid Number: \(gridColumn)")
+        
+        //Toolbar Image
+        switch gridColumn {
+        case 1:
+            toolbarIcon = "square.grid.2x2"
+        case 2:
+            toolbarIcon = "square.grid.3x2"
+        case 3:
+            toolbarIcon = "rectangle.grid.1x2"
+        default:
+            toolbarIcon = "square.grid.2x2"
+        }
+    }
     
     //MARK: - BODY
     var body: some View {
-        let animals: [Animal] = Bundle.main.decode("animals.json")
         NavigationView {
-            List {
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                ForEach(animals) { animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
-                        AnimalListItemView(animal: animal)
-                    } //: Link
-                } //: Loop
-            } //: List
+            Group {
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        ForEach(animals) { animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                AnimalListItemView(animal: animal)
+                            } //: Link
+                        } //: Loop
+                        CreditsView()
+                            .modifier(CenterModifier())
+                    } //: List
+                } else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                            ForEach(animals) { animal in
+                                NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                    AnimalGridItemView(animal: animal)
+                                } //: Link
+                            } //: Loop
+                        } //: Grid
+                        .padding(10)
+                        .animation(.easeIn)
+                    } //: Scroll
+                } //: Condition
+                
+            } //: Group
             .navigationBarTitle("Africa", displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        //List
+                        Button(action: {
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                        }
+                        
+                        //Grid
+                        Button(action: {
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                            gridSwitch()
+                        }) {
+                            Image(systemName: toolbarIcon)
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                        }
+                    } //: HStack
+                } //: Buttons
+            } //: Toolbar
         } //: NavigationView
     }
 }
--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,10 +6,85 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
     var body: some View {
-        Text("Map")
+        //MARK: - 1. BASIC MAP
+//        Map(coordinateRegion: $region)
+        
+        //MARK: - 2. ADVANCED MAP
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { item in
+            //(A) Pin: Old Style (always static)
+//            MapPin(coordinate: item.location, tint: .accentColor)
+            //(B) Marker: New Style (always static)
+//            MapMarker(coordinate: item.location, tint: .accentColor)
+            //(C) Custom Basic Annotation (it could be interactive)
+//            MapAnnotation(coordinate: item.location) {
+//                Image("logo")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .frame(width: 32, height: 32, alignment: .center)
+//            } //: MapAnnotation
+            //(D) Custom advanced annotation (it could be interactive)
+            MapAnnotation(coordinate: item.location) {
+                MapAnnotationView(location: item)
+            }
+        }) //: Map
+        .overlay(
+            HStack(alignment: .center, spacing: 12) {
+                Image("compass")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 48, height: 48, alignment: .center)
+                
+                VStack(alignment: .leading, spacing: 3) {
+                    HStack {
+                        Text("Latitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    } //: HStack
+                    
+                    Divider()
+                    
+                    HStack {
+                        Text("Longitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.longitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    } //: HStack
+                } //: VStack
+            } //: HStack
+            .padding(.vertical, 12)
+            .padding(.horizontal, 16)
+            .background(
+                Color.black
+                    .cornerRadius(8)
+                    .opacity(0.6)
+            )
+            .padding()
+            , alignment: .top
+        )
     }
 }
 
